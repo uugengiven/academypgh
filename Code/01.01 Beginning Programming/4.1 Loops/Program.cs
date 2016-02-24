dffac93b47f9d02347adc89d6fee748dad9d8722
uugengiven <lange.john.m@gmail.com>
2016-02-24 13:45:46 -0500

    Add loops

---
+++ Code/01.01 Beginning Programming/4.1 Loops/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (true)
+            {
+                Console.WriteLine("This will happen once.");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("This will happen over and over, forever.");
+                // try to not do this
+                // but you /can/ get out of this with a break statement
+                break;
+            }
+           
+
+
+            //exercise stuff
+
+            var conditional = true;
+            var i = 0;
+            while (conditional)
+            {
+                Console.WriteLine("The value of i is " + i);
+                i = i + 1;
+                if (i > 10)
+                {
+                    conditional = false;
+                }
+            }
+
+            conditional = true;
+            var input = "";
+            while (conditional)
+            {
+                Console.WriteLine("I will repeat until you type: stop");
+                input = Console.ReadLine();
+                if (input == "stop")
+                {
+                    conditional = false;
+                }
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Code/01.01 Beginning Programming/4.2 For Loops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("The value of i is " + i);
+            }
+
+
+            Console.ReadLine();
+
+            int i = 0;
+            int numberOfLoops = 100;
+
+            for (; i < numberOfLoops; i++)
+            {
+                // you can use other variables, you do not have to declare them in the for declaration
+                // this will run 100 times, just like the previous one
+                // why 100 and not 101 if i starts at 0?
+            }
+        }
+    }
+}
